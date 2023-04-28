e699ebaa40da17d75ee05b42b522290ddab2bd26
Anna Kreidina <kav99mail@gmail.com>
2023-04-28 23:18:28 +0300

    marcup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Button } from './Button/Button';
+import { Statistics } from './Statistics/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handelClick = e => {
+    console.log(e);
+    this.setState();
+  };
+  render() {
+    const btns = Object.keys(this.state);
+    return (
+      <div>
+        <Button btns={btns} handelClick={this.handelClick} />
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.state.good}
+          positivePercentage={this.state.good}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
